35aca72e396b69fbec653572e0c271fe9525692a
Thallys Rocha <thallyschrist@gmail.com>
2023-04-24 09:33:20 -0300

    Modularizado

--- rpn.py
+++ rpn.py
@@ -1,30 +1,22 @@
-from Token import Token
+from scan import scan
 
 stack = []
-tokens = []
-f = open("Calc1.stk", "r")
-for line in [l.rstrip() for l in f.readlines()]:    
-    try:
-        tokens.append(Token(line))
-        index = len(tokens) - 1   
-        print(tokens[index].toString())   
-        #print(index)
-        if(tokens[index].type == "NUM"):
-            stack.append(int(tokens[index].lexeme))
-        elif(len(stack)>=2):
-            if(tokens[index].type == "STAR"):
-                stack.append(stack.pop()*stack.pop())
-            elif(tokens[index].type == "SLASH"):
-                stack.append(int(1/stack.pop()*stack.pop()))
-            elif(tokens[index].type == "MINUS"):
-                stack.append((-stack.pop()) + stack.pop())
-            elif(tokens[index].type == "PLUS"):
-                stack.append(stack.pop()+stack.pop())
-        else:
-            print("Não há números o suficiente para executar as operações, o último valor calculado foi: "+str(stack.pop()))
-            break
-    except Exception as error:
-        raise Exception(error)  
+tokens = scan("Calc1.stk")
+for token in tokens:
+    if(token.type == "NUM"):
+        stack.append(int(token.lexeme))
+    elif(len(stack)>=2):
+        if(token.type == "STAR"):
+            stack.append(stack.pop()*stack.pop())
+        elif(token.type == "SLASH"):
+            stack.append(int(1/stack.pop()*stack.pop()))
+        elif(token.type == "MINUS"):
+            stack.append((-stack.pop()) + stack.pop())
+        elif(token.type == "PLUS"):
+            stack.append(stack.pop()+stack.pop())
+    else:
+        print("Não há números o suficiente para executar as operações, o último valor calculado foi: "+str(stack.pop()))
+        break
     
 
 if(len(stack) == 1):
